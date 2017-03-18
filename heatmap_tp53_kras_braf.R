7765e2ce86d0e43c5fcaf45171c1b11ef947d276
Roberto Tirado Magallanes <rtmag4@gmail.com>
2017-03-18 22:16:35 +0800

    Update heatmap_tp53_kras_braf.R

--- heatmap_tp53_kras_braf.R
+++ heatmap_tp53_kras_braf.R
@@ -25,8 +25,7 @@ meth.norm<-meth(rnb.set.norm)
 #            (tp53_dmr_table$diffmeth.p.adj.fdr<0.05 & abs(tp53_dmr_table$mean.diff)>.15 ) | 
 #            (braf_dmr_table$diffmeth.p.adj.fdr<0.05 & abs(braf_dmr_table$mean.diff)>.15 ) | 
 #            (kras_dmr_table$diffmeth.p.adj.fdr<0.05 & abs(kras_dmr_table$mean.diff)>.15 ) ))
-x=which(
-            tp53_dmr_table$diffmeth.p.adj.fdr<0.05 & abs(tp53_dmr_table$mean.diff)>.15  )
+x=which(  tp53_dmr_table$diffmeth.p.adj.fdr<0.01 & abs(tp53_dmr_table$mean.diff)>.05 )
 
 meth.norm.sig=meth.norm[x,]
 
@@ -61,11 +60,11 @@ cols=brewer.pal(3, "Set1")
 hclustfunc <- function(x) hclust(x, method="complete")
 distfunc <- function(x) dist(x, method="euclidean")
 
-tiff("COAD_FDR-1_methDif-05.tiff",res = 300)
+#tiff("COAD_FDR-1_methDif-05.tiff",res = 300)
 x=heatmap.3(meth.norm.sig,col=colors, hclustfun=hclustfunc, distfun=distfunc, 
             scale="none", trace="none",cexCol=0.2,KeyValueName="Methylation Level",
-             ColSideColors=clab,Colv=F,dendrogram="both")
-dev.off()
+             ColSideColors=clab,Colv=T,dendrogram="both")
+#dev.off()
   
   
   
