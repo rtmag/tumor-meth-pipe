05015b9701b5a3e03421ac8be6b859c1a46cc1d1
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-04-20 01:07:15 +0800

    Update ALL_heatmap.bash

--- ALL_heatmap.bash
+++ ALL_heatmap.bash
@@ -13,3 +13,32 @@ cd ../../scripts/ ;
 done
 
 
+Rscript /root/TCGA/TCGA/scripts/all_heatmap.R  /root/TCGA/TCGA/preprocessing_tp53/ACC/ &
+Rscript /root/TCGA/TCGA/scripts/all_heatmap.R  /root/TCGA/TCGA/preprocessing_tp53/CESC/ &
+Rscript /root/TCGA/TCGA/scripts/all_heatmap.R  /root/TCGA/TCGA/preprocessing_tp53/CHOL/ &
+Rscript /root/TCGA/TCGA/scripts/all_heatmap.R  /root/TCGA/TCGA/preprocessing_tp53/COAD/ &
+Rscript /root/TCGA/TCGA/scripts/all_heatmap.R  /root/TCGA/TCGA/preprocessing_tp53/ESCA/ &
+Rscript /root/TCGA/TCGA/scripts/all_heatmap.R  /root/TCGA/TCGA/preprocessing_tp53/GBM/ &
+Rscript /root/TCGA/TCGA/scripts/all_heatmap.R  /root/TCGA/TCGA/preprocessing_tp53/HNSC/ &
+Rscript /root/TCGA/TCGA/scripts/all_heatmap.R  /root/TCGA/TCGA/preprocessing_tp53/KICH/ &
+Rscript /root/TCGA/TCGA/scripts/all_heatmap.R  /root/TCGA/TCGA/preprocessing_tp53/KIRC/ &
+Rscript /root/TCGA/TCGA/scripts/all_heatmap.R  /root/TCGA/TCGA/preprocessing_tp53/KIRP/ &
+Rscript /root/TCGA/TCGA/scripts/all_heatmap.R  /root/TCGA/TCGA/preprocessing_tp53/LAML/ &
+Rscript /root/TCGA/TCGA/scripts/all_heatmap.R  /root/TCGA/TCGA/preprocessing_tp53/LGG/ &
+Rscript /root/TCGA/TCGA/scripts/all_heatmap.R  /root/TCGA/TCGA/preprocessing_tp53/LIHC/ &
+Rscript /root/TCGA/TCGA/scripts/all_heatmap.R  /root/TCGA/TCGA/preprocessing_tp53/LUAD/ &
+Rscript /root/TCGA/TCGA/scripts/all_heatmap.R  /root/TCGA/TCGA/preprocessing_tp53/LUSC/ &
+Rscript /root/TCGA/TCGA/scripts/all_heatmap.R  /root/TCGA/TCGA/preprocessing_tp53/MESO/ &
+Rscript /root/TCGA/TCGA/scripts/all_heatmap.R  /root/TCGA/TCGA/preprocessing_tp53/PAAD/ &
+Rscript /root/TCGA/TCGA/scripts/all_heatmap.R  /root/TCGA/TCGA/preprocessing_tp53/PCPG/ &
+Rscript /root/TCGA/TCGA/scripts/all_heatmap.R  /root/TCGA/TCGA/preprocessing_tp53/PRAD/ &
+Rscript /root/TCGA/TCGA/scripts/all_heatmap.R  /root/TCGA/TCGA/preprocessing_tp53/READ/ &
+Rscript /root/TCGA/TCGA/scripts/all_heatmap.R  /root/TCGA/TCGA/preprocessing_tp53/SARC/ &
+Rscript /root/TCGA/TCGA/scripts/all_heatmap.R  /root/TCGA/TCGA/preprocessing_tp53/SKCM/ &
+Rscript /root/TCGA/TCGA/scripts/all_heatmap.R  /root/TCGA/TCGA/preprocessing_tp53/STAD/ &
+Rscript /root/TCGA/TCGA/scripts/all_heatmap.R  /root/TCGA/TCGA/preprocessing_tp53/TGCT/ &
+Rscript /root/TCGA/TCGA/scripts/all_heatmap.R  /root/TCGA/TCGA/preprocessing_tp53/THCA/ &
+Rscript /root/TCGA/TCGA/scripts/all_heatmap.R  /root/TCGA/TCGA/preprocessing_tp53/THYM/ &
+Rscript /root/TCGA/TCGA/scripts/all_heatmap.R  /root/TCGA/TCGA/preprocessing_tp53/UCEC/ &
+Rscript /root/TCGA/TCGA/scripts/all_heatmap.R  /root/TCGA/TCGA/preprocessing_tp53/UCS/ &
+Rscript /root/TCGA/TCGA/scripts/all_heatmap.R  /root/TCGA/TCGA/preprocessing_tp53/UVM/ &
