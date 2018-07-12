04491e71e5d44dba640e5784860cc21ca65c41c8
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-07-13 03:53:30 +0800

    Update RNA_tcga_coad.bash

--- RNA/RNA_tcga_coad.bash
+++ RNA/RNA_tcga_coad.bash
@@ -3,3 +3,6 @@ more gdc_sample_sheet.2018-07-13.tsv |cut -f1,2,7|perl -pe 's/\t/\//g'|perl -pe 
 more sample.annotation_tp53_kras_braf.txt |cut -f1|perl -pe 's/\-/\t/g'|cut -f1-4|perl -pe 's/\t/\-/g' > 450k_annotation_tcga.txt
 
 more sample.txt |grep -f 450k_annotation_tcga.txt
+
+more sample.annotation_tp53_kras_braf.txt |cut -f1|perl -pe 's/\-/\t/g'|cut -f1-4|perl -pe 's/\t/\-/g' | \
+paste - sample.annotation_tp53_kras_braf.txt |cut -f1,4 > 450k_annotation_tcga_p53.txt
