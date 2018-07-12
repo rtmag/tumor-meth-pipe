c7389f90c84b5673852d5518a100e7382f869a02
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-07-13 03:43:37 +0800

    Create RNA_tcga_coad.bash

---
+++ RNA/RNA_tcga_coad.bash
@@ -0,0 +1,5 @@
+
+more gdc_sample_sheet.2018-07-13.tsv |cut -f1,2,7|perl -pe 's/\t/\//g'|perl -pe 's/gz\//gz\t/g' > sample.txt
+more sample.annotation_tp53_kras_braf.txt |cut -f1|perl -pe 's/\-/\t/g'|cut -f1-4|perl -pe 's/\t/\-/g' > 450k_annotation_tcga.txt
+
+more sample.txt |grep -f 450k_annotation_tcga.txt
