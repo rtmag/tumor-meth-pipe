8debde57226a867f49ed183604ed9d3764add5bb
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-07-13 05:02:56 +0800

    Update rna_tcga.R

--- RNA/rna_tcga.R
+++ RNA/rna_tcga.R
@@ -1,1 +1,35 @@
+library(gplots)
+library(ggplot2)
+library(graphics)
+library(scales)
+library(vegan)
+options(scipen=999)
+library(gplots)
+library(factoextra)
+library(RColorBrewer)
+library("xlsx")
+library(beeswarm)
 
+rna_samples = read.table("sample.txt",sep='\t')
+rna_samples = cbind(rna_samples,rna_samples)
+
+sample_450k = read.table("450k_annotation_tcga_p53.txt",sep='\t',header=TRUE)
+
+#match(a,b) # a[ !is.na(match) ]  # b[which(match)]
+ix= match(rna_samples[,1], sample_450k[,1])
+
+#head(rna_samples[!is.na(ix),])
+#head(sample_450k[ix[!is.na(ix)],])
+
+rna = rownames(rna_samples)[!is.na(ix)]
+k450 = sample_450k[ix[!is.na(ix)],]
+
+rpkm <- do.call(cbind, lapply(rna, function(x) read.table(pipe(paste('zcat',x) ), sep="\t", row.names=1 ) ))
+
+ix_foxm1 = grep("ENSG00000111206",rownames(rpkm))
+
+FOXM1 = data.frame(cell = as.character(k450[,2]), gene=log2(as.numeric(rpkm[ix_foxm1,])) )
+
+boxplot(gene ~ cell, vertical = TRUE, data = FOXM1,ylab = expression('Log2 FOXM1 TPM'),names=c("TP53 Mutant","Normal","TP53 WT"),outline=F)
+stripchart(gene ~ cell, vertical = TRUE, data = FOXM1, jitter = 0.3, ylab = expression('Log2 FOXM1 TPM'),
+    method = "jitter", pch = 20, col = alpha(colour='red',alpha=.5),cex = 2,add=T)
