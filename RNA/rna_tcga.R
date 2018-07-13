992032939669be5a07145b7c65ef5e6c503cbaa9
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-07-13 12:12:12 +0800

    Update rna_tcga.R

--- RNA/rna_tcga.R
+++ RNA/rna_tcga.R
@@ -29,7 +29,19 @@ rpkm <- do.call(cbind, lapply(rna, function(x) read.table(pipe(paste('zcat',x) )
 ix_foxm1 = grep("ENSG00000111206",rownames(rpkm))
 
 FOXM1 = data.frame(cell = as.character(k450[,2]), gene=log2(as.numeric(rpkm[ix_foxm1,])) )
-
-boxplot(gene ~ cell, vertical = TRUE, data = FOXM1,ylab = expression('Log2 FOXM1 TPM'),names=c("TP53 Mutant","Normal","TP53 WT"),outline=F)
-stripchart(gene ~ cell, vertical = TRUE, data = FOXM1, jitter = 0.3, ylab = expression('Log2 FOXM1 TPM'),
+pdf("TCGA_FOXM1.pdf")
+boxplot(gene ~ cell, vertical = TRUE, data = FOXM1,ylab = expression('Log2 FOXM1 FPKM'),names=c("TP53 Mutant","Normal","TP53 WT"),outline=F)
+stripchart(gene ~ cell, vertical = TRUE, data = FOXM1, jitter = 0.3, ylab = expression('Log2 FOXM1 FPKM'),
     method = "jitter", pch = 20, col = alpha(colour='red',alpha=.5),cex = 2,add=T)
+dev.off()
+#ENSG00000124762
+ix_CDKN1 = grep("ENSG00000124762",rownames(rpkm))
+                              
+CDKN1 = data.frame(cell = as.character(k450[,2]), gene=log2(as.numeric(rpkm[ix_CDKN1,])) )
+pdf("TCGA_CDKN1.pdf")
+boxplot(gene ~ cell, vertical = TRUE, data = CDKN1,ylab = expression('Log2 CDKN1A FPKM'),names=c("TP53 Mutant","Normal","TP53 WT"),outline=F)
+stripchart(gene ~ cell, vertical = TRUE, data = CDKN1, jitter = 0.3, ylab = expression('Log2 CDKN1A FPKM'),
+    method = "jitter", pch = 20, col = alpha(colour='red',alpha=.5),cex = 2,add=T)
+dev.off()
+
+                              
